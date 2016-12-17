a520d7eb7bac80950dfa54f08c3a8c12e175c742
Tiago Loriato <tloriatosimoes@gmail.com>
2016-12-17 02:12:05 -0200

    Update shuffle_deck.js

--- shuffle_deck.js
+++ shuffle_deck.js
@@ -1,3 +1,21 @@
+var deck = [
+            "AceC",        "AceD",          "AceH",          "AceS",
+            "2C",            "2D",            "2H",            "2S",
+            "3C",            "3D",            "3H",            "3S",
+            "4C",            "4D",            "4H",            "4S",
+            "5C",            "5D",            "5H",            "5S",
+            "6C",            "6D",            "6H",            "6S",
+            "7C",            "7D",            "7H",            "7S",
+            "8C",            "8D",            "8H",            "8S",
+            "9C",            "9D",            "9H",            "9S",  
+            "10C",           "10D",           "10H",           "10S",  
+            "JC",            "JD",            "JH",            "JS",  
+            "QC",            "QD",            "QH",            "QS",  
+            "KC",            "KD",            "KH",            "KS" 
+            ];
+
+deck = shuffle(deck);
+
 function shuffle(array) {
   var num = array.length, t, i;
 
@@ -15,3 +33,5 @@ function shuffle(array) {
   
   return array;
 }
+
+console.log(deck);
